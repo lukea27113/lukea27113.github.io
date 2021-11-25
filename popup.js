8b48cf4ef8480017f0e6d894585ea137e99c3a94
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2021-11-25 13:26:18 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,23 @@
+function checkCookie() {
+  let isVerified = getCookie("isVerified");
+  if (isVerified != "true") {
+  document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+}); 
+  } else {
+      window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+    
+}
+end
+
 window.addEventListener("load", function(){
     setTimeout(
         function open(event){
